9e69b8791ee91ff93e8bb61af1db75f7ab473b8f
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2026-02-05 14:52:34 +0600

    Friends server is ready to take post request and resoponse for get request

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ app.get("/", (req, res) => {
   res.send("Hy Here Friend Management Service");
 });
 
+const Friends=[
+  {id:1,name:"JAVA",email:"john.doe@example.com"},
+  {id:2,name:"JAVA SCRIPT",email:"jane.smith@example.com"},
+  {id:3,name:"Python",email:"alice.johnson@example.com"},
+  {id:4,name:"Flutter Dart",email:"bob.brown@example.com"}
+]
+
+app.get("/friends",(req,res)=>{
+  res.json(Friends);
+})
+
+app.post('/friends',(req,res)=>{
+  const newFriend = req.body;
+  console.log(newFriend);
+  newFriend.id = Friends.length +1;
+  Friends.push(newFriend);
+  res.send(newFriend);
+
+  res.json({message:"Friend added successfully"})
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
